e4a43c134b61dd7b5be29f9aa84dc3f14c52985d
Duglas Pineda <00116921@uca.edu.sv>
2026-02-14 13:50:49 -0600

    Initial Commit

---
+++ OpenCal/OpenCalApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenCalApp.swift
+//  OpenCal
+//
+//  Created by Fernando Pineda on 14/2/26.
+//
+
+import SwiftUI
+
+@main
+struct OpenCalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
